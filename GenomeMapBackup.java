87809026cb6f6fdcd9cc8588f2183385f23f8e98
Pavan Govu <70082921+pavangovu@users.noreply.github.com>
2021-02-02 23:11:05 -0600

    Add files via upload

--- GenomeMapBackup.java
+++ GenomeMapBackup.java
@@ -1,19 +1,25 @@
-import ca.ualberta.stothard.cgview.*;
+//Java Standard Template Library
 import java.io.*;
 import java.util.*;
 import java.nio.file.*;
+
+//Cgview Library
+import ca.ualberta.stothard.cgview.*;
 import java.awt.*;
 
 class GenomeMapBackup implements CgviewConstants
 { 
    public static void main( String args[] ) throws FileNotFoundException
    {
+      //genbank input file 
       File genomeFile = new File("Genome.gb");
    
-      Scanner input = new Scanner(genomeFile);        
-      boolean sequenceFound=false;
+      //necessary local variables
+      Scanner input = new Scanner(genomeFile);
+      boolean sequenceFound=false;                                   
       java.util.List<String> sequences = new ArrayList<String>();
          
+      //parse through .gb file
       while(input.hasNext())
       {
          String currentLine=input.nextLine();
@@ -27,55 +33,58 @@ class GenomeMapBackup implements CgviewConstants
             }
          }
          
+         //beginning of DNA sequence
          if(chopped[0].equals("ORIGIN"))
             sequenceFound=true;
       }
       
+      //for testing purposes only
       //System.out.println(sequences.get(sequences.size()-1));   
       
       int length = 2765;
       Cgview cgview = new Cgview(length);
       
-      //some optional settings
-      cgview.setWidth(1350);
-      cgview.setHeight(1080);
-      cgview.setBackboneRadius(350.0f);
-      cgview.setTitle("Tomato Curly Stunt Virus");
-      cgview.setTitleFont(new Font("SansSerif", Font.BOLD, 22));
-      cgview.setLabelPlacementQuality(20);
+      //Genome Map Configurations
+      cgview.setWidth(1350);  //optimal width of png
+      cgview.setHeight(1080); //omptimal height of png
+      cgview.setBackboneRadius(350.0f);            //radius of circular genome map
+      cgview.setTitle("Tomato Curly Stunt Virus"); //title
+      cgview.setTitleFont(new Font("SansSerif", Font.BOLD, 22)); //title font
+      cgview.setLabelPlacementQuality(20);   
       cgview.setShowWarning(false);
+      
+      //label specifications
       cgview.setLabelLineLength(8.0d);
       cgview.setLabelLineThickness(0.5f);
    
-      //create a FeatureSlot to hold sequence features
+      //FeatureSlot to hold sequence features
       FeatureSlot featureSlot = new FeatureSlot(cgview, DIRECT_STRAND);
    
-      //create random sequence features
+      //load sequences
       int alternate=1;
-      
       for (int i = 0; i <sequences.size(); i++) {
-         int j = Math.round((float)((float)(length - 2) * Math.random())) + 1;
          
-         //a Feature to add to our FeatureSlot
-         Feature feature = new Feature(featureSlot, sequences.get(i)+""+i);
+         //new feature
+         Feature feature = new Feature(featureSlot, sequences.get(i));
          
+         //alternate label colors for increased readability
          if(alternate==1)
          {
-            feature.setColor(Color.blue);
+            feature.setColor(Color.blue);//blue
             alternate++;
          }
          else if(alternate==2)
          {
-            feature.setColor(Color.red);
+            feature.setColor(Color.red);//red
             alternate++;
          } 
          else if(alternate==3)
          {
-            feature.setColor(Color.green);
-            alternate=1;
+            feature.setColor(Color.green);//green
+            alternate=1;//reset color
          }
          
-         //a single FeatureRange to add the Feature
+         //length of feature sector
          int start=0, stop=0;
          
          if(i==276)
@@ -89,19 +98,23 @@ class GenomeMapBackup implements CgviewConstants
             start=i*10+1;
             stop=(i+1)*10;
          }
-         FeatureRange featureRange = new FeatureRange (feature,start, stop);
          
-         featureRange.setDecoration(DECORATION_CLOCKWISE_ARROW);
-          
+         //draw feature onto genome map
+         FeatureRange featureRange = new FeatureRange (feature,start, stop);
+         featureRange.setDecoration(DECORATION_CLOCKWISE_ARROW);//each feature demarcated by arrow
       }
       
-      try {
-         //create a PNG file
+      try 
+      {
+         //PNG File
          CgviewIO.writeToPNGFile(cgview, ".\\CgviewTest1.png");
-         System.exit(0);
+         
+         //testing purposes only
+         //System.exit(0);
       }
-      catch (IOException e) {
-         e.printStackTrace(System.err);
+      catch (IOException e)//handle possible PNG exception 
+      {
+         e.printStackTrace(System.err);//see what the error was
          System.exit(1);
       }
    }
